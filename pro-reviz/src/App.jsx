4c5af06b4bd603c599302d1a7da0eea5fa670448
XFillD <haubafilip@gmail.com>
2023-09-29 19:25:39 +0200

    Update

--- pro-reviz/src/App.jsx
+++ pro-reviz/src/App.jsx
@@ -4,42 +4,17 @@ import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 import Cards from "./components/Cards";
 import Portfolio from "./components/Portfolio";
-import { useInView } from "react-intersection-observer";
-import { Transition } from "@headlessui/react";
 import UnderConstruction from "./components/UnderConstruction";
-
-const AnimatedComponent = ({ children }) => {
-  const [ref, inView] = useInView({
-    triggerOnce: true,
-    rootMargin: "-50px 0px",
-    threshold: 0.1,
-  });
-
-  return (
-    <div ref={ref}>
-      <Transition
-        show={inView}
-        as="div"
-        enter="transform transition-all duration-1000 ease-in-out"
-        enterFrom="opacity-0 translate-y-full"
-        enterTo="opacity-100 translate-y-0"
-      >
-        {children}
-      </Transition>
-    </div>
-  );
-};
 
 function App() {
   return (
     <div className="bg-gradient-to-br from-gray-900 to-gray-700">
-      {/* <Navbar />
+      <Navbar />
       <Hero />
       <Cards />
       <hr className="border-black border-solid" />
       <Portfolio />
-      <Footer />  */}
-      <UnderConstruction />
+      <Footer />
     </div>
   );
 }
